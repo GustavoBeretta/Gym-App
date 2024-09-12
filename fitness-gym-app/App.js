85871bde79e8ac778aa4dad86a6dc604ed2ab8c2
Josef Bruseke <josefbruseke@gmail.com>
2024-09-12 15:26:36 -0300

    add gym details route

--- fitness-gym-app/App.js
+++ fitness-gym-app/App.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import NewsScreen from './screens/NewsScreen'; 
+import NewsScreen from './screens/NewsScreen';
 import TutorialsScreen from './screens/TutorialsScreen';
-import GymsScreen from './screens/GymsScreen'; 
+import GymsScreen from './screens/GymsScreen';
 import Navbar from './components/NavBarr';
 import Header from './components/Header';
 import { View } from 'react-native';
+import GymDetails from './screens/GymDetails';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,12 +25,16 @@ export default function App() {
         <Stack.Screen
           name="Gyms"
           component={GymsScreen}
-          options={{ title: 'Details Page' }}
         />
         <Stack.Screen
           name="Tutorials"
           component={TutorialsScreen}
         />
+        <Stack.Screen
+          name="Gym Details"
+          component={GymDetails}
+        />
+
       </Stack.Navigator>
       <Navbar />
     </NavigationContainer>
