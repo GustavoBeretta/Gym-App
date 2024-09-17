77b75d8f7b857f9aa60f372f8a15c738fcc0651e
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-09-16 21:42:06 -0300

    phone app link

--- fitness-gym-app/screens/GymDetails.js
+++ fitness-gym-app/screens/GymDetails.js
@@ -3,31 +3,26 @@ import { View, Text, Image, StyleSheet, Dimensions, ScrollView } from 'react-nat
 import Icon from 'react-native-vector-icons/FontAwesome'
 import openLink from '../utils';
 
-const { width, height } = Dimensions.get('window')
+const { width } = Dimensions.get('window')
 
 const GymDetails = ({ route }) => {
     const { gym } = route.params;
-
 
     return (
         <ScrollView>
             <Image source={{ uri: gym.image }} style={styles.image} />
             <View style={styles.container}>
-
                 <Text style={styles.gymName}>{gym.name}</Text>
                 <Text style={styles.gymAddress}>{gym.address}</Text>
-                <Text style={styles.phone}>{gym.contact_phone}</Text>
+                <Text style={styles.phone} onPress={() => openLink(`tel:${gym.contact_phone}`)}>{gym.contact_phone}</Text>
                 <Text style={styles.gymHours}>Monday-Friday: {gym.hours.monday_to_friday}</Text>
                 <Text style={styles.gymHours}>Saturday: {gym.hours.saturday}</Text>
                 <Text style={styles.gymHours}>Sunday: {gym.hours.sunday}</Text>
-
                 <Text style={styles.membership}>Average Price: {gym.membership_price}</Text>
                 <View style={styles.icons}>
                     <Icon name='whatsapp' size={50} onPress={() => openLink(gym.whatsapp)} color="green" />
                     <Icon name='map-marker' color="red" size={50} onPress={() => openLink(`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(gym.address)}`)} />
-
                 </View>
-
             </View>
         </ScrollView>
     );
@@ -40,7 +35,6 @@ const styles = StyleSheet.create({
     },
     container: {
         padding: 20,
-        /*flex: 'flex-row',*/
         alignItems: 'center',
         gap: 2,
     },
